e5d942ad7ea5444a2d93eba0d1ba925d5d4b13e3
Leonardo Fabio Mercado Benítez <lfmercadobe@unal.edu.co>
2020-12-16 05:40:06 -0500

    Se implementa satisfactoriamente el reconocimiento de las cajas

--- proyecto_final_rcsv/scripts/ball_detection.py
+++ proyecto_final_rcsv/scripts/ball_detection.py
@@ -7,8 +7,16 @@ from std_msgs.msg           import String
 from sensor_msgs.msg        import Image
 from geometry_msgs.msg      import Point
 from cv_bridge              import CvBridge, CvBridgeError
+
+caja_1_clasificador = cv2.CascadeClassifier('/home/leonardo/Documentos/01_RCSV/05_Proyecto_Final/Proyecto_final_RCSV/proyecto_final_rcsv/scripts/caja_1_clasificador.xml')
+
 azul_min = np.array([110,100,20],np.uint8)
 azul_max = np.array([130,255,255],np.uint8)
+
+x_l_1, y_l_1 = 0, 0
+x_l_2, y_l_2 = 570, 720
+x_r_1, y_r_1 = 750, 0
+x_r_2, y_r_2 = 1280, 720
 
 
 class image_receive:
@@ -29,6 +37,7 @@ class image_receive:
         except CvBridgeError as e:
             print(e)
 
+        gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
         img_hsv = cv2.cvtColor(cv_image,cv2.COLOR_BGR2HSV) # Se convierte al espacio HSV
         mascara_1 = cv2.inRange(img_hsv,azul_min,azul_max)
         mascara_color = cv2.bitwise_and(cv_image,cv_image, mask=mascara_1)
@@ -43,7 +52,7 @@ class image_receive:
                 y = int(M['m01']/M['m00'])
                 cv2.circle(cv_image,(x,y),7,(0,0,255),2)
                 fuente = cv2.FONT_HERSHEY_SIMPLEX
-                cv2.putText(cv_image, 'centro en [{};{}]'.format(x,y),(50,50), fuente, 0.75,(0,255,0),2,cv2.LINE_AA)
+                cv2.putText(cv_image, '[{};{}]'.format(x,y),(580,25), fuente, 0.75,(0,255,0),2,cv2.LINE_AA)
                 cv2.drawContours(cv_image, [c],0,(255,0,0),3)
                 self.puntos.x = x
                 self.puntos.y = y
@@ -53,6 +62,20 @@ class image_receive:
 
         cv2.line(cv_image, (639,342), (739,342), (0,0,255), 2)
         cv2.line(cv_image, (639,342), (639,442), (0,255,0), 2)
+
+        
+        caja_1 = caja_1_clasificador.detectMultiScale(gray, scaleFactor = 10, minNeighbors = 150,minSize=(70,78))
+
+        
+        for (x,y,w,h) in caja_1:
+            if (x<x_l_2 and y<y_l_2):
+                cv2.rectangle(cv_image,(x_l_1,y_l_1),(x_l_2,y_l_2),(0,0,255),2)
+                cv2.putText(cv_image,'Caja',(10,20),2,0.7,(0,0,255),2,cv2.LINE_AA)
+            elif (x>x_r_1 and y<y_r_2):
+                cv2.rectangle(cv_image,(x_r_1,y_r_1),(x_r_2,y_r_2),(0,0,255),2)
+                cv2.putText(cv_image,'Caja',(1140,20),2,0.7,(0,0,255),2,cv2.LINE_AA)
+        
+        
         cv2.imshow('deteccion',cv_image)
         #cv2.imshow('En HSV',img_hsv)
         #cv2.imshow('Filtrado HSV',mascara_1)
